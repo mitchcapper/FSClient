12e3c910ea0ff4fe55f4f856505e1fa957f9adad
Mitch Capper <mitch.capper@gmail.com>
2011-07-08 11:41:41 -0700

    Fixed ext-rtp-ip to not default to stun.freeswitch.org, made FSEvent use C++ get header func again

--- FSEvent.cs
+++ FSEvent.cs
@@ -10,12 +10,12 @@ namespace FSClient {
 			this.evt = evt;
 		}
 		public string get_header(string name) {
-			//return freeswitch.switch_event_get_header(evt, name); //removed as for some reason its not swigging right for now...
-			for (var x = evt.headers; x != null; x = x.next){
-				if (name.ToLower() == x.name.ToLower())
-					return x.value;
-			}
-			return null;
+			return freeswitch.switch_event_get_header_idx(evt, name, -1);
+			//for (var x = evt.headers; x != null; x = x.next){
+			//    if (name.ToLower() == x.name.ToLower())
+			//        return x.value;
+			//}
+			//return null;
 		}
 		public string body { get { return evt.body; } }
 		public int flags { get { return evt.flags; } }
